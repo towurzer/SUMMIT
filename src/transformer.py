f5739166a97eb2c64358685ad867be1a375438ff
towurzer <t.wurzer.2004@gmail.com>
2024-12-28 13:49:30 +0100

    renamed variables and methods according to conventions

--- src/transformer.py
+++ src/transformer.py
@@ -5,11 +5,11 @@ import torch.nn as nn
 
 
 class InputEmbeddings(nn.Module):
-    def __init__(self, dimensions: int, vocabSize: int, *args, **kwargs):
+    def __init__(self, dimensions: int, vocab_size: int):
         super().__init__()
         self.dimensions = dimensions
-        self.vocabSize = vocabSize
-        self.embedding = nn.Embedding(vocabSize, dimensions)  # method from pytorch for embedding
+        self.vocabSize = vocab_size
+        self.embedding = nn.Embedding(vocab_size, dimensions)  # method from pytorch for embedding
 
     def forward(self, pos):
         return self.embedding(pos) * math.sqrt(self.dimensions)  # scaling
@@ -17,15 +17,15 @@ class InputEmbeddings(nn.Module):
 
 
 class PositionalEncodings(nn.Module):
-    def __init__(self, dimensions: int, maxSequenceLength: int, dropout: float):
+    def __init__(self, dimensions: int, max_sequence_length: int, dropout: float):
         super().__init__()
         self.dimensions = dimensions
-        self.maxSequenceLength = maxSequenceLength
+        self.max_sequence_length = max_sequence_length
         self.dropout = nn.Dropout(dropout)
 
         # create positional Encodings
-        positionalEncodings = torch.zeros(maxSequenceLength, dimensions)  # 2D tensor (len, dimensions)
-        position = torch.arange(0, maxSequenceLength, 1, dtype=torch.float).unsqueeze(1) # (maxSequenceLength, 1)
+        positional_encodings = torch.zeros(max_sequence_length, dimensions)  # 2D tensor (len, dimensions)
+        position = torch.arange(0, max_sequence_length, 1, dtype=torch.float).unsqueeze(1) # (maxSequenceLength, 1)
 
         # Compute the divisor as 10^(4^(2i/d))
         i = torch.arange(0, dimensions, 2, dtype=torch.float)  # Indices for even dimensions
@@ -33,14 +33,14 @@ class PositionalEncodings(nn.Module):
         # TODO: Use numerically stable logarithmic formula to avoid large number problems
         x = position / divisor
 
-        positionalEncodings[:, 0::2] = torch.sin(x)
-        positionalEncodings[:, 1::2] = torch.cos(x)
+        positional_encodings[:, 0::2] = torch.sin(x)
+        positional_encodings[:, 1::2] = torch.cos(x)
 
-        positionalEncodings = positionalEncodings.unsqueeze(0)  # Add a batch dimension to the positional encoding
-        self.register_buffer("positionalEncodings", positionalEncodings)
-        # adds a new parameter self.positionalEncodings which isn't learned but saved and loaded in training
+        positional_encodings = positional_encodings.unsqueeze(0)  # Add a batch dimension to the positional encoding
+        self.register_buffer("positional_encodings", positional_encodings)
+        # adds a new parameter self.positional_encodings which isn't learned but saved and loaded in training
 
     def forward(self, x):
-        # x = x + positionalEncoding of x
-        x = x + (self.positionalEncodings[:, :x.shape[1], :]).requires_grad_(False)  # (batch, seq_len, dimensions)
+        # x = x + positional_encodings of x
+        x = x + (self.positional_encodings[:, :x.shape[1], :]).requires_grad_(False)  # (batch, seq_len, dimensions)
         return self.dropout(x)
