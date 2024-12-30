865d37df79eb092b9b3dea9a2b82ec5d67c69156
Cap1l <129529494+Cap1l@users.noreply.github.com>
2024-12-30 08:05:22 +0100

    Update dataset.py

--- src/dataset.py
+++ src/dataset.py
@@ -11,7 +11,7 @@ class DataLoader():
 
     @staticmethod
     def get_dataset(config):
-    #split='train' is chosen because of errors occuring or the splits not being loaded when choosing another
+    #split='train' is chosen because of errors occuring or the splits not being loaded when choosing another.
     #('train' is 1m lines while test and val are 2k each) https://huggingface.co/datasets/Helsinki-NLP/opus-100/viewer/de-en/train
         dataset = load_dataset(f"{config['datasource']}", f"{config['lang_source']}-{config['lang_target']}", split='train')
 
@@ -29,16 +29,12 @@ class DataLoader():
 #Testing if it works
 train_ds, validation_ds, test_ds = DataLoader.get_dataset(get_config())
 
-fixit = DataLoader()
-
-train2_ds, validation2_ds, test2_ds = fixit.get_dataset(get_config())
-
 #Data points printed are the amount of sentence pairs
-print(f"Train dataset size: {len(train2_ds)}")
-print(f"Validation dataset size: {len(validation2_ds)}")
-print(f"Test dataset size: {len(test2_ds)}\n")
+print(f"Train dataset size: {len(train_ds)}")
+print(f"Validation dataset size: {len(validation_ds)}")
+print(f"Test dataset size: {len(test_ds)}\n")
 
 for i in range(3):
-    print(f"{i+1}: {train2_ds[i]}")
+    print(f"{i+1}: {train_ds[i]}")
 
 
